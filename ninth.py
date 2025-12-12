e49df3ac058d708347a5b125ed1890aa8a5b1083
Vítek Hošek <hosekv01@jcu.cz>
2025-12-12 11:39:37 +0000

    psuh

---
+++ ninth.py
@@ -0,0 +1,23 @@
+def dec_to_bin(cislo):
+    # funkce prevede cislo na binarni reprezentaci (cislo muze byt str i int!!!)
+    # 7 -> "111"
+    # 5 -> "101"
+    if isinstance(cislo, str):
+        cislo = int(cislo)
+    if cislo == 0:
+        return "0"
+    if cislo > 0:
+        binarni = ""
+        while cislo > 0:
+            binarni = str(cislo % 2) + binarni
+            cislo = cislo // 2
+        return binarni
+
+
+def test_bin_to_dec():
+    assert dec_to_bin("0") == "0"
+    assert dec_to_bin(1) == "1"
+    assert dec_to_bin("100") == "1100100"
+    assert dec_to_bin(101) == "1100101"
+    assert dec_to_bin(127) == "1111111"
+    assert dec_to_bin("128") == "10000000"
